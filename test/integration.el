7ab05d66f6ae477e921241be7245ee4bbfbc2c2c
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2023-07-04 19:42:13 +0200

    Add tests for HTML, Java, JS, TS, PHP

--- test/integration.el
+++ test/integration.el
@@ -4,15 +4,18 @@
 (require 'lsp-sonarlint)
 (require 'lsp-sonarlint-python)
 
-(defun lsp-sonarlint--get-issues (file invoke-lsp-mode)
-  (let ((dir (file-name-directory file)))
+(defun lsp-sonarlint--get-issues (file knob-symbol)
+  (let ((lsp-enabled-clients '(sonarlint))
+        (dir (file-name-directory file)))
     (lsp-workspace-folders-add dir)
     (unwind-protect
         (let ((buf (find-file-noselect file))
               (result nil)
               (lsp-sonarlint-plugin-autodownload t))
           (with-current-buffer buf
-            (funcall invoke-lsp-mode)
+            (cl-letf (((symbol-value knob-symbol) t))
+              (python-mode) ;; Any of lsp-sonarlint-modes-enabled enable all lsp-sonarlint languages
+              (lsp))
             (let ((diagnostics-updated nil))
               (cl-flet ((check-diags-for-file ()
                                               (when-let ((stats (lsp-diagnostics-stats-for file)))
@@ -20,37 +23,48 @@
                                                   (setq diagnostics-updated t)))))
                 (add-hook 'lsp-diagnostics-updated-hook #'check-diags-for-file)
                 (unwind-protect
-                    (with-timeout (5) (while (not diagnostics-updated) (sit-for 0.1 t)))
-                  (remove-hook 'lsp-diagnostics-updated-hook #'check-diags-for-file)))
-              )
+                    (with-timeout (10) (while (not diagnostics-updated) (sit-for 0.1 t)))
+                  (remove-hook 'lsp-diagnostics-updated-hook #'check-diags-for-file))))
             (setq result (gethash file (lsp-diagnostics t))))
           (kill-buffer buf)
           result)
-      ; TODO: lsp-workspace-shutdown?
       (lsp-workspace-folders-remove dir))))
 
 (defun lsp-sonarlint--get-issue-codes (issues)
   (sort (mapcar (lambda (issue) (gethash "code" issue)) issues) #'string-lessp))
 
-(defun lsp-sonarlint--invoke-python-lsp-mode ()
-  (let ((lsp-sonarlint-python-enabled t))
-    (python-mode)
-    (lsp)))
-
 (ert-deftest lsp-sonarlint-python-reports-issues ()
   (let ((issues (lsp-sonarlint--get-issues "/home/arseniy/proj/lsp-sonarlint/fixtures/sample.py"
-                                        #'lsp-sonarlint--invoke-python-lsp-mode)))
+                                           'lsp-sonarlint-python-enabled)))
     (should (equal (lsp-sonarlint--get-issue-codes issues)
                    '("python:S1135" "python:S1716")))))
 
-
-(defun lsp-sonarlint--invoke-java-lsp-mode ()
-  (let ((lsp-sonarlint-java-enabled t))
-    (python-mode)
-    (lsp)))
-
 (ert-deftest lsp-sonarlint-java-reports-issues ()
   (let ((issues (lsp-sonarlint--get-issues "/home/arseniy/proj/lsp-sonarlint/fixtures/sample.java"
-                                        #'lsp-sonarlint--invoke-java-lsp-mode)))
+                                        'lsp-sonarlint-java-enabled)))
     (should (equal (lsp-sonarlint--get-issue-codes issues)
                    '("java:S106" "java:S1135" "java:S1220")))))
+
+(ert-deftest lsp-sonarlint-html-reports-issues ()
+  (let ((issues (lsp-sonarlint--get-issues "/home/arseniy/proj/lsp-sonarlint/fixtures/sample.html"
+                                           'lsp-sonarlint-html-enabled)))
+    (should (equal (lsp-sonarlint--get-issue-codes issues)
+                   '("Web:S1135")))))
+
+(ert-deftest lsp-sonarlint-js-reports-issues ()
+  (let ((issues (lsp-sonarlint--get-issues "/home/arseniy/proj/lsp-sonarlint/fixtures/javascript-sample.js"
+                                           'lsp-sonarlint-javascript-enabled)))
+    (should (equal (lsp-sonarlint--get-issue-codes issues)
+                   '("javascript:S1134" "javascript:S1135")))))
+
+(ert-deftest lsp-sonarlint-ts-reports-issues ()
+  (let ((issues (lsp-sonarlint--get-issues "/home/arseniy/proj/lsp-sonarlint/fixtures/sample.ts"
+                                           'lsp-sonarlint-typescript-enabled)))
+    (should (equal (lsp-sonarlint--get-issue-codes issues)
+                   '("typescript:S1134" "typescript:S1135")))))
+
+(ert-deftest lsp-sonarlint-php-reports-issues ()
+  (let ((issues (lsp-sonarlint--get-issues "/home/arseniy/proj/lsp-sonarlint/fixtures/sample.php"
+                                           'lsp-sonarlint-php-enabled)))
+    (should (equal (lsp-sonarlint--get-issue-codes issues)
+                   '("php:S1135" "php:S1780")))))
