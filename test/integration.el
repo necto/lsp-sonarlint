dbcc112235e6d3d7cf38f0637485bd29686b0b57
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2023-07-04 08:49:08 +0200

    Integration test for sonarlint java plugin

--- test/integration.el
+++ test/integration.el
@@ -15,18 +15,22 @@
             (funcall invoke-lsp-mode)
             (let ((diagnostics-updated nil))
               (cl-flet ((check-diags-for-file ()
-                                              (when (lsp-diagnostics-stats-for file)
-                                                (setq diagnostics-updated t))))
+                                              (when-let ((stats (lsp-diagnostics-stats-for file)))
+                                                (when (< 0 (seq-reduce '+ stats 0))
+                                                  (setq diagnostics-updated t)))))
                 (add-hook 'lsp-diagnostics-updated-hook #'check-diags-for-file)
                 (unwind-protect
                     (with-timeout (5) (while (not diagnostics-updated) (sit-for 0.1 t)))
-                  (remove-hook 'lsp-diagnostics-updated-hook #'check-diags-for-file))))
+                  (remove-hook 'lsp-diagnostics-updated-hook #'check-diags-for-file)))
+              )
             (setq result (gethash file (lsp-diagnostics t))))
           (kill-buffer buf)
           result)
       ; TODO: lsp-workspace-shutdown?
       (lsp-workspace-folders-remove dir))))
 
+(defun lsp-sonarlint--get-issue-codes (issues)
+  (sort (mapcar (lambda (issue) (gethash "code" issue)) issues) #'string-lessp))
 
 (defun lsp-sonarlint--invoke-python-lsp-mode ()
   (let ((lsp-sonarlint-python-enabled t))
@@ -36,7 +40,17 @@
 (ert-deftest lsp-sonarlint-python-reports-issues ()
   (let ((issues (lsp-sonarlint--get-issues "/home/arseniy/proj/lsp-sonarlint/fixtures/sample.py"
                                         #'lsp-sonarlint--invoke-python-lsp-mode)))
-    (should (equal (length issues) 2))
-    (should (equal (gethash "code" (nth 0 issues)) "python:S1135"))
-    (should (equal (gethash "code" (nth 1 issues)) "python:S1716"))))
+    (should (equal (lsp-sonarlint--get-issue-codes issues)
+                   '("python:S1135" "python:S1716")))))
 
+
+(defun lsp-sonarlint--invoke-java-lsp-mode ()
+  (let ((lsp-sonarlint-java-enabled t))
+    (python-mode)
+    (lsp)))
+
+(ert-deftest lsp-sonarlint-java-reports-issues ()
+  (let ((issues (lsp-sonarlint--get-issues "/home/arseniy/proj/lsp-sonarlint/fixtures/sample.java"
+                                        #'lsp-sonarlint--invoke-java-lsp-mode)))
+    (should (equal (lsp-sonarlint--get-issue-codes issues)
+                   '("java:S106" "java:S1135" "java:S1220")))))
