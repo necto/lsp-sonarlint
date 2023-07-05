b0cea84673901f1a09855d28939ef8b24f8287ec
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2023-07-05 09:07:18 +0200

    Run the integration tests on CI

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ SHELL := /usr/bin/env bash
 EMACS ?= emacs
 EASK ?= eask
 
-TEST-FILES := $(shell ls test/lsp-sonarlint-*.el)
+TEST-FILES := $(shell ls test/*.el)
 
 .PHONY: clean checkdoc lint package install compile test
 
@@ -23,7 +23,7 @@ compile:
 
 test:
 	@echo "Testing..."
-	$(EASK) exec ert-runner -L . $(LOAD-TEST-FILES) -t '!no-win' -t '!org'
+	$(EASK) test ert $(TEST-FILES)
 
 clean:
 	$(EASK) clean all
--- test/lsp-sonarlint-integration-test.el
+++ test/lsp-sonarlint-integration-test.el
@@ -3,12 +3,40 @@
 (require 'lsp-mode)
 (require 'lsp-sonarlint)
 (require 'lsp-sonarlint-python)
+(require 'lsp-sonarlint-java)
+(require 'lsp-sonarlint-html)
+(require 'lsp-sonarlint-javascript)
+(require 'lsp-sonarlint-typescript)
+(require 'lsp-sonarlint-php)
+(require 'lsp-sonarlint-xml)
+(require 'lsp-sonarlint-text)
+(require 'lsp-sonarlint-go)
 
-(defun lsp-sonarlint--get-issues (file knob-symbol &optional mode)
+(defun lsp-sonarlint--wait-for (predicate hook timeout)
+  (let ((done nil))
+    (cl-flet ((setter (lambda () (when (funcall predicate)
+                              (setq done t)))))
+      (unwind-protect
+          (progn
+            (add-hook hook #'setter)
+            (with-timeout (timeout) (while (not done) (sit-for 0.1 t))))
+        (remove-hook hook #'setter)))))
+
+(defun lsp-sonarlint--get-issues (file knob-symbol)
   ;; Any of lsp-sonarlint-modes-enabled enable all lsp-sonarlint languages
-  (setq mode (or mode #'python-mode))
   (let ((lsp-enabled-clients '(sonarlint))
-        (dir (file-name-directory file)))
+        (dir (file-name-directory file))
+        ;; Disable all plugins to focus only on the issues from the knob-symbol
+        (lsp-sonarlint-go-enabled nil)
+        (lsp-sonarlint-html-enabled nil)
+        (lsp-sonarlint-java-enabled nil)
+        (lsp-sonarlint-javascript-enabled nil)
+        (lsp-sonarlint-typescript-enabled nil)
+        (lsp-sonarlint-php-enabled nil)
+        (lsp-sonarlint-text-enabled nil)
+        (lsp-sonarlint-typescript-enabled nil)
+        (lsp-sonarlint-xml-enabled nil)
+        )
     (lsp-workspace-folders-add dir)
     (unwind-protect
         (let ((buf (find-file-noselect file))
@@ -16,19 +44,19 @@
               (lsp-sonarlint-plugin-autodownload t))
           (with-current-buffer buf
             (cl-letf (((symbol-value knob-symbol) t))
-              (funcall mode)
-              (lsp))
-            (let ((diagnostics-updated nil))
-              (cl-flet ((check-diags-for-file ()
-                                              (when-let ((stats (lsp-diagnostics-stats-for file)))
-                                                (when (< 0 (seq-reduce '+ stats 0))
-                                                  (setq diagnostics-updated t)))))
-                (add-hook 'lsp-diagnostics-updated-hook #'check-diags-for-file)
-                (unwind-protect
-                    (with-timeout (10) (while (not diagnostics-updated) (sit-for 0.1 t)))
-                  (remove-hook 'lsp-diagnostics-updated-hook #'check-diags-for-file))))
+              ;; (print knob-symbol)
+              ;; (print lsp-sonarlint-java-enabled)
+              (python-mode)
+              (lsp)) ;; TODO: wait for finishing the init? lsp-after-initialize-hook
+            (lsp-sonarlint--wait-for
+             (lambda ()
+               (when-let ((stats (lsp-diagnostics-stats-for file)))
+                 (when (< 0 (seq-reduce '+ stats 0))
+                   (setq diagnostics-updated t))))
+             'lsp-diagnostics-updated-hook
+             10)
             (setq result (gethash file (lsp-diagnostics t))))
-          (kill-buffer buf)
+          (kill-buffer buf) ;; TODO: wait for finishing the teardown? lsp-after-uninitialized-functions lsp-unconfigure-hook
           result)
       (lsp-workspace-folders-remove dir))))
 
@@ -82,10 +110,9 @@
 ;; "text" plugin detects secrets and bidirectional unicode characters
 (ert-deftest lsp-sonarlint-text-reports-issues ()
   (let ((issues (lsp-sonarlint--get-issues "/home/arseniy/proj/lsp-sonarlint/fixtures/secrets.java"
-                                           'lsp-sonarlint-text-enabled
-                                           #'java-mode)))
+                                           'lsp-sonarlint-text-enabled)))
     (should (equal (lsp-sonarlint--get-issue-codes issues)
-                   '("java:S1118" "java:S1220" "secrets:S6290" "secrets:S6290" "secrets:S6290")))))
+                   '("secrets:S6290" "secrets:S6290" "secrets:S6290")))))
 
 (ert-deftest lsp-sonarlint-go-reports-issues ()
   (let ((issues (lsp-sonarlint--get-issues "/home/arseniy/proj/lsp-sonarlint/fixtures/sample.go"
