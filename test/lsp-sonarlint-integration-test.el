21d84ac44914638fedf2bbfbcc9137702c5fe459
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2023-07-05 18:53:15 +0200

    Fix the paths: use relative ones

--- test/lsp-sonarlint-integration-test.el
+++ test/lsp-sonarlint-integration-test.el
@@ -2,15 +2,6 @@
 
 (require 'lsp-mode)
 (require 'lsp-sonarlint)
-(require 'lsp-sonarlint-python)
-(require 'lsp-sonarlint-java)
-(require 'lsp-sonarlint-html)
-(require 'lsp-sonarlint-javascript)
-(require 'lsp-sonarlint-typescript)
-(require 'lsp-sonarlint-php)
-(require 'lsp-sonarlint-xml)
-(require 'lsp-sonarlint-text)
-(require 'lsp-sonarlint-go)
 
 (defun lsp-sonarlint--wait-for (predicate hook timeout)
   (let ((done nil))
@@ -35,8 +26,7 @@
         (lsp-sonarlint-php-enabled nil)
         (lsp-sonarlint-text-enabled nil)
         (lsp-sonarlint-typescript-enabled nil)
-        (lsp-sonarlint-xml-enabled nil)
-        )
+        (lsp-sonarlint-xml-enabled nil))
     (lsp-workspace-folders-add dir)
     (unwind-protect
         (let ((buf (find-file-noselect file))
@@ -44,9 +34,7 @@
               (lsp-sonarlint-plugin-autodownload t))
           (with-current-buffer buf
             (cl-letf (((symbol-value knob-symbol) t))
-              ;; (print knob-symbol)
-              ;; (print lsp-sonarlint-java-enabled)
-              (python-mode)
+              (python-mode) ;; Any prog mode that triggers lsp-sonarlint triggers all its analyzers
               (lsp)) ;; TODO: wait for finishing the init? lsp-after-initialize-hook
             (lsp-sonarlint--wait-for
              (lambda ()
@@ -63,20 +51,33 @@
 (defun lsp-sonarlint--get-issue-codes (issues)
   (sort (mapcar (lambda (issue) (gethash "code" issue)) issues) #'string-lessp))
 
+(defun lsp-sonarlint--fixtures-dir ()
+  (concat
+   (file-name-directory
+    (directory-file-name (file-name-directory (symbol-file #'lsp-sonarlint--fixtures-dir))))
+   "fixtures/"))
+
+(defun lsp-sonarlint--sample-file (fname)
+  (concat (lsp-sonarlint--fixtures-dir) fname))
+
+;; TODO: make the paths relative!
 (ert-deftest lsp-sonarlint-python-reports-issues ()
-  (let ((issues (lsp-sonarlint--get-issues "/home/arseniy/proj/lsp-sonarlint/fixtures/sample.py"
+  (require 'lsp-sonarlint-python)
+  (let ((issues (lsp-sonarlint--get-issues (lsp-sonarlint--sample-file "sample.py")
                                            'lsp-sonarlint-python-enabled)))
     (should (equal (lsp-sonarlint--get-issue-codes issues)
                    '("python:S1135" "python:S1716")))))
 
 (ert-deftest lsp-sonarlint-java-reports-issues ()
-  (let ((issues (lsp-sonarlint--get-issues "/home/arseniy/proj/lsp-sonarlint/fixtures/sample.java"
+  (require 'lsp-sonarlint-java)
+  (let ((issues (lsp-sonarlint--get-issues (lsp-sonarlint--sample-file "sample.java")
                                         'lsp-sonarlint-java-enabled)))
     (should (equal (lsp-sonarlint--get-issue-codes issues)
                    '("java:S106" "java:S1135" "java:S1220")))))
 
 (ert-deftest lsp-sonarlint-html-reports-issues ()
-  (let ((issues (lsp-sonarlint--get-issues "/home/arseniy/proj/lsp-sonarlint/fixtures/sample.html"
+  (require 'lsp-sonarlint-html)
+  (let ((issues (lsp-sonarlint--get-issues (lsp-sonarlint--sample-file "sample.html")
                                            'lsp-sonarlint-html-enabled)))
     (should (equal (lsp-sonarlint--get-issue-codes issues)
                    '("Web:S1135")))))
@@ -84,38 +85,44 @@
 ;; javascript-sample.js must have a distinct name from sample.ts,
 ;; otherwise the javascript/typescript plugin gets confused.
 (ert-deftest lsp-sonarlint-js-reports-issues ()
-  (let ((issues (lsp-sonarlint--get-issues "/home/arseniy/proj/lsp-sonarlint/fixtures/javascript-sample.js"
+  (require 'lsp-sonarlint-javascript)
+  (let ((issues (lsp-sonarlint--get-issues (lsp-sonarlint--sample-file "javascript-sample.js")
                                            'lsp-sonarlint-javascript-enabled)))
     (should (equal (lsp-sonarlint--get-issue-codes issues)
                    '("javascript:S1134" "javascript:S1135")))))
 
 (ert-deftest lsp-sonarlint-ts-reports-issues ()
-  (let ((issues (lsp-sonarlint--get-issues "/home/arseniy/proj/lsp-sonarlint/fixtures/sample.ts"
+  (require 'lsp-sonarlint-typescript)
+  (let ((issues (lsp-sonarlint--get-issues (lsp-sonarlint--sample-file "sample.ts")
                                            'lsp-sonarlint-typescript-enabled)))
     (should (equal (lsp-sonarlint--get-issue-codes issues)
                    '("typescript:S1134" "typescript:S1135")))))
 
 (ert-deftest lsp-sonarlint-php-reports-issues ()
-  (let ((issues (lsp-sonarlint--get-issues "/home/arseniy/proj/lsp-sonarlint/fixtures/sample.php"
+  (require 'lsp-sonarlint-php)
+  (let ((issues (lsp-sonarlint--get-issues (lsp-sonarlint--sample-file "sample.php")
                                            'lsp-sonarlint-php-enabled)))
     (should (equal (lsp-sonarlint--get-issue-codes issues)
                    '("php:S1135" "php:S1780")))))
 
 (ert-deftest lsp-sonarlint-xml-reports-issues ()
-  (let ((issues (lsp-sonarlint--get-issues "/home/arseniy/proj/lsp-sonarlint/fixtures/sample.xml"
+  (require 'lsp-sonarlint-xml)
+  (let ((issues (lsp-sonarlint--get-issues (lsp-sonarlint--sample-file "sample.xml")
                                            'lsp-sonarlint-xml-enabled)))
     (should (equal (lsp-sonarlint--get-issue-codes issues)
                    '("xml:S1135")))))
 
 ;; "text" plugin detects secrets and bidirectional unicode characters
 (ert-deftest lsp-sonarlint-text-reports-issues ()
-  (let ((issues (lsp-sonarlint--get-issues "/home/arseniy/proj/lsp-sonarlint/fixtures/secrets.java"
+  (require 'lsp-sonarlint-text)
+  (let ((issues (lsp-sonarlint--get-issues (lsp-sonarlint--sample-file "secrets.java")
                                            'lsp-sonarlint-text-enabled)))
     (should (equal (lsp-sonarlint--get-issue-codes issues)
                    '("secrets:S6290" "secrets:S6290" "secrets:S6290")))))
 
 (ert-deftest lsp-sonarlint-go-reports-issues ()
-  (let ((issues (lsp-sonarlint--get-issues "/home/arseniy/proj/lsp-sonarlint/fixtures/sample.go"
+  (require 'lsp-sonarlint-go)
+  (let ((issues (lsp-sonarlint--get-issues (lsp-sonarlint--sample-file "sample.go")
                                            'lsp-sonarlint-go-enabled)))
     (should (equal (lsp-sonarlint--get-issue-codes issues)
                    '("go:S1135")))))
